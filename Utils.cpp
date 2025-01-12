66f466362170ff34877ff624d4ae5958e615c06d
Dax0s <dev@dklimavicius.com>
2025-01-12 16:45:19 +0200

    Add ClearWhitespace function to utils

--- Utils.cpp
+++ Utils.cpp
@@ -7,3 +7,19 @@ int FindChar(const std::string &s, const char ch) {
 
     return -1;
 }
+
+std::string ClearWhitespace(const std::string &s) {
+    std::string result;
+    result.reserve(s.length());
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != ' ') {
+            result += s[i];
+        }
+    }
+
+    int a = s.length();
+    int b = result.length();
+
+    return result;
+}
--- Utils.h
+++ Utils.h
@@ -5,4 +5,6 @@
 
 int FindChar(const std::string &s, char ch);
 
+std::string ClearWhitespace(const std::string &s);
+
 #endif //UTILS_H
