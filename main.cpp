05ac00204cbcee7b0182361027efb4bff5a3a943
Dax0s <dev@dklimavicius.com>
2025-01-12 20:52:50 +0200

    Add main assembler loop

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <fstream>
 
 #include "BinaryConverter.h"
 #include "Parser.h"
 #include "Utils.h"
 
-int main() {
-    std::cout << BinaryConverter::CommandToBin("   @   2   522\n") << std::endl;
-    std::cout << BinaryConverter::CommandToBin("AMD=D+1;JMP\n") << std::endl;
+int main(const int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: ./HackAssembler <file>" << std::endl;
+        return 1;
+    }
+
+    std::string outputFileName = argv[1];
+    outputFileName = outputFileName.substr(0, outputFileName.length() - 4);
+    outputFileName += ".hack";
+
+    if (std::ifstream inputFile(argv[1]); inputFile.is_open()) {
+        if (std::ofstream outputFile(outputFileName); outputFile.is_open()) {
+            std::string line;
+            while (getline(inputFile, line)) {
+                std::string clearLine = ClearWhitespace(line);
+                if (clearLine.substr(0, 2) != "//" && !clearLine.empty()) {
+                    outputFile << BinaryConverter::CommandToBin(line) << std::endl;
+                }
+            }
+            outputFile.close();
+        }
+        inputFile.close();
+    } else {
+        std::cerr << "Failed to open file " << argv[1] << std::endl;
+    }
 
     return 0;
 }
